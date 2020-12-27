73f14622b60778aa262064166ffb4bff31de6d64
Manabu Niseki <manabu.niseki@gmail.com>
2020-12-27 14:53:12 +0900

    refactor: disable onyphe's favicon search

--- frontend/src/components/links/Onyphe.vue
+++ frontend/src/components/links/Onyphe.vue
@@ -14,17 +14,32 @@
 import * as qs from "qs";
 import { Component, Prop, Vue } from "vue-property-decorator";
 
-import { Hashes } from "@/types";
+import { Hashes, QueryType } from "@/types";
 
 @Component
 export default class Onyphe extends Vue {
   @Prop() hashes!: Hashes;
 
+  get queryType(): QueryType {
+    if (this.hashes.contentType.includes("text/html")) {
+      return "html";
+    }
+    return "favicon";
+  }
+
   get query(): string {
+    if (this.queryType === "favicon") {
+      return "N/A";
+    }
+
     return `category:datascan app.http.bodymd5:"${this.hashes.md5}"`;
   }
 
   get link(): string {
+    if (this.query === "N/A") {
+      return "/";
+    }
+
     const baseUrl = "https://onyphe.io/search/?";
     const params = {
       query: this.query,
