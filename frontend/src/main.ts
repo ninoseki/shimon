2745be0ffa31f735c15b9a5b3dbbed2f7e0bd2ad
Manabu Niseki <manabu.niseki@gmail.com>
2022-11-26 09:22:30 +0900

    chore: fix frontend issues

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,6 @@ import "bulma/css/bulma.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 
-// Polyfill
-import "regenerator-runtime/runtime";
-
 import { createApp } from "vue";
 
 import App from "./App.vue";
